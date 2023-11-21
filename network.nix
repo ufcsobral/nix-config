4fadbac42e78a6060d60eb99d4c47bdb4a947ad6
Almino Melo <almino@sobral.ufc.br>
2023-11-21 17:27:59 -0300

    Tinha esquecido do SSH

--- network.nix
+++ network.nix
@@ -1,12 +1,19 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    # ports = [ 2398 ];
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = lib.mkDefault true;
+    };
+  };
 }
