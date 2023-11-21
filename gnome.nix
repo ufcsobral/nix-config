7c1e8e22e45e9fae707597b191574ef24fd2c996
Almino Melo <almino@sobral.ufc.br>
2023-11-21 17:59:40 -0300

    Algumas coisas que podem ser úteis no GNOME

--- gnome.nix
+++ gnome.nix
@@ -1,6 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    adw-gtk3 # https://github.com/lassekongo83/adw-gtk3
+    adwaita-qt
+    dconf
+    gnome-obfuscate
+    gnome.adwaita-icon-theme
+    gnome.gnome-software
+    gnome.gnome-tweaks
+    gnomeExtensions.expandable-notifications
+  ];
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
