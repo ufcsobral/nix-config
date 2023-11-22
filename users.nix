81c3777c01d76b2959846f26f10bd9ad33a87f22
Almino Melo <almino@sobral.ufc.br>
2023-11-22 13:47:29 -0300

    Melhorando os usuários

--- users.nix
+++ users.nix
@@ -1,18 +1,35 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users = {
-    dti = {
-      isNormalUser = true;
-      description = "DTI";
-      extraGroups = [ "networkmanager" "wheel" ];
-      initialHashedPassword = "$y$j9T$v5pc.sLoYn26GUDibRrBA0$fdVBYV7sU0F3iwxUX9P80EIBAPmqAPq/tgzh5DEyZkD";
-    };
-    visitante = {
-      isNormalUser = true;
-      description = "Visitante";
-      extraGroups = [ "networkmanager" ];
-    };
+  # services.accounts-daemon.enable = lib.mkDefault true;
+
+  # https://nixos.org/manual/nixos/stable/options#opt-system.activationScripts
+  system.activationScripts = {
+    visitante = "passwd -d visitante";
   };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users =
+    let
+      groups = [ "networkmanager" ];
+      password = "$y$j9T$v5pc.sLoYn26GUDibRrBA0$fdVBYV7sU0F3iwxUX9P80EIBAPmqAPq/tgzh5DEyZkD";
+      with-sudo = groups ++ [ "wheel" ];
+    in
+    {
+      dti = {
+        createHome = lib.mkDefault true;
+        description = "DTI";
+        extraGroups = with-sudo;
+        group = "users";
+        home = "/home/dti";
+        initialHashedPassword = password;
+        isSystemUser = lib.mkDefault true;
+      };
+      visitante = {
+        description = "Visitante";
+        extraGroups = groups;
+        initialHashedPassword = password;
+        isNormalUser = true;
+      };
+    };
 }
