5d2b5ef7664abe358496ea73ad97297539946ba9
Almino Melo <almino@sobral.ufc.br>
2023-11-22 11:11:03 -0300

    Senha padrão da DTI

--- users.nix
+++ users.nix
@@ -7,6 +7,7 @@
       isNormalUser = true;
       description = "DTI";
       extraGroups = [ "networkmanager" "wheel" ];
+      initialHashedPassword = "$y$j9T$v5pc.sLoYn26GUDibRrBA0$fdVBYV7sU0F3iwxUX9P80EIBAPmqAPq/tgzh5DEyZkD";
     };
     visitante = {
       isNormalUser = true;
